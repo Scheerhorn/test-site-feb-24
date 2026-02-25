debd3343670637e21f6566b588ed75622f98e0d6
Scheerhorn <daniel.scheerhorn.wolfe@gmail.com>
2026-02-24 19:36:31 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById("actionBtn").addEventListener("click", function() {
+    document.getElementById("message").textContent = 
+        "You just used JavaScript to change the page!";
+});
